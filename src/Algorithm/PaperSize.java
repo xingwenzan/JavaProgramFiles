fa717f4d38c8e958f2ab84e7f0b037695f83fa28
86159 <zxw3180716935@163.com>
2023-01-06 23:44:37 +0800

    Add
    Signed-off-by: 86159 <zxw3180716935@163.com>

---
+++ src/Algorithm/PaperSize.java
@@ -0,0 +1,20 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class PaperSize {
+    static int[] a = new int[11];
+
+    public static void main(String[] args) {
+        a[0] = 1189;
+        a[1] = 841;
+        for (int i = 2; i < 11; i++) {
+            a[i] = Math.max(a[i - 2], a[i - 1]) / 2;
+        }
+        Scanner scanner = new Scanner(System.in);
+        String n0 = scanner.nextLine();
+        int n = Integer.parseInt(n0.charAt(1) + "");
+        System.out.println(a[n]);
+        System.out.println(a[n + 1]);
+    }
+}
