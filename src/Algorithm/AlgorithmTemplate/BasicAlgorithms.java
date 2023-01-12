7b36708992afefcbafaaed6d9c001beaf2aea4b6
86159 <zxw3180716935@163.com>
2023-01-13 01:22:42 +0800

    Add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmTemplate/BasicAlgorithms.java
+++ src/Algorithm/AlgorithmTemplate/BasicAlgorithms.java
@@ -1,23 +1,23 @@
 package Algorithm.AlgorithmTemplate;
 
-// 基础算法模板/快速调用
+// 基础算法模板/快速调用   // 未调好
 public class BasicAlgorithms {
 
-    // 快速排序，https://www.acwing.com/problem/content/787/
+    // 快速排序 O(nlogn) https://www.acwing.com/problem/content/787/
     public static void quickSort(int[] lst, int l, int r) {
         if (l >= r) {
             return;
         }
-        int i = l;
-        int j = r;
+        int i = l - 1;
+        int j = r + 1;
         int x = lst[(l + r) >> 1];
         while (i < j) {
-            while (lst[i] < x) {
+            do {
                 i++;
-            }
-            while (lst[j] > x) {
+            } while (lst[i] < x);
+            do {
                 j--;
-            }
+            } while (lst[j] > x);
             if (i < j) {
                 int k = lst[i];
                 lst[i] = lst[j];
@@ -29,5 +29,33 @@ public class BasicAlgorithms {
         }
     }
 
-    //
+    // 快速选择 O(n) https://www.acwing.com/problem/content/description/788/
+    public static int quickChoose(int[] lst, int l, int r, int k) {
+        if (l >= r) {
+            return lst[l];
+        }
+        int i = l - 1;
+        int j = r + 1;
+        int x = lst[(l + r) >> 1];
+        while (i < j) {
+            do {
+                i++;
+            } while (lst[i] < x);
+            do {
+                j--;
+            } while (lst[j] > x);
+            if (i < j) {
+                int z = lst[i];
+                lst[i] = lst[j];
+                lst[j] = z;
+            }
+
+            if (j - l + 1 >= k) {
+                return quickChoose(lst, l, j, k);
+            } else {
+                return quickChoose(lst, j + 1, r, k - (j - l + 1));
+            }
+        }
+        return 0;
+    }
 }
--- src/Algorithm/AlgorithmTemplate/Main.java
+++ src/Algorithm/AlgorithmTemplate/Main.java
@@ -5,19 +5,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+//    public static void main(String[] args) throws IOException {// 快排
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        String s = bufferedReader.readLine();
+//        int n = Integer.parseInt(s.charAt(0) + ""); // 读单个字符并转数字
+//        //System.out.println(i+1);
+//        String[] strings = bufferedReader.readLine().split(" ");
+//        int[] lst = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            lst[i] = Integer.parseInt(strings[i]);
+//        }
+//        BasicAlgorithms.quickSort(lst, 0, n - 1); // 快速排序
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(lst[i] + " ");
+//        }
+//    }
+
+    public static void main(String[] args) throws IOException {// 快选
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String s = bufferedReader.readLine();
-        int n = Integer.parseInt(s.charAt(0) + ""); // 读单个字符并转数字
-        //System.out.println(i+1);
+        String s[] = bufferedReader.readLine().split(" ");
+        int[] input = new int[2];
+        for (int i = 0; i < 2; i++) {
+            input[i] = Integer.parseInt(s[i]);
+        }
+        int n = input[0];
+        int k = input[1];
         String[] strings = bufferedReader.readLine().split(" ");
         int[] lst = new int[n];
         for (int i = 0; i < n; i++) {
             lst[i] = Integer.parseInt(strings[i]);
         }
-        BasicAlgorithms.quickSort(lst, 0, n - 1); // 快速排序
-        for (int i = 0; i < n; i++) {
-            System.out.print(lst[i] + " ");
-        }
+        int ans = BasicAlgorithms.quickChoose(lst, 0, n - 1, k); // 快速排序
+        System.out.println(ans);
     }
 }
