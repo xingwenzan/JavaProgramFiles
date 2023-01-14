5889d0a7b85c93840a0077e0a11f6cb51911b74b
86159 <zxw3180716935@163.com>
2023-01-15 00:02:01 +0800

    Add
    Signed-off-by: 86159 <zxw3180716935@163.com>

---
+++ src/Algorithm/DeleteSequence.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DeleteSequence {
+    // 题目：删除序列；链接：https://www.acwing.com/problem/content/4799/
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        long[] value = new long[100010];
+        long[] score = new long[100010];
+        String[] strings = bufferedReader.readLine().split(" ");
+        for (String s : strings) {
+            int i = Integer.parseInt(s);
+            value[i] += i;
+        }
+        for (int i = 1; i < 100010; i++) {
+            score[i] = Math.max(score[i - 1], score[Math.max(0, i - 2)] + value[i]);
+        }
+        System.out.println(score[100010 - 1]);
+    }
+}
