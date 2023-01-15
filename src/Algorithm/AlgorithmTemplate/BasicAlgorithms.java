bec4d18cf1bdc014d367bb9a6e0f04128afbbfc2
86159 <zxw3180716935@163.com>
2023-01-15 23:53:22 +0800

    Add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmTemplate/BasicAlgorithms.java
+++ src/Algorithm/AlgorithmTemplate/BasicAlgorithms.java
@@ -53,4 +53,34 @@ public class BasicAlgorithms {
             return quickChoose(lst, j + 1, r, k - (j - l + 1));
         }
     }
+
+    // 归并排序 0(nlogn) https://www.acwing.com/problem/content/789/
+    public static void mergeSort(int[] lst, int l, int r) {
+        if (l >= r) return;
+        int mid = l + r >> 1;
+        mergeSort(lst, l, mid);
+        mergeSort(lst, mid + 1, r);
+
+        int i = l, j = mid + 1, k = 0;
+        int[] tmp = new int[r - l + 1];
+
+        while (i <= mid && j <= r) {
+            if (lst[i] <= lst[j]) {
+                tmp[k++] = lst[i++];
+            } else {
+                tmp[k++] = lst[j++];
+            }
+        }
+        while (i <= mid) {
+            tmp[k++] = lst[i++];
+        }
+        while (j <= r) {
+            tmp[k++] = lst[j++];
+        }
+
+        for (int a = l, b = 0; a <= r; a++, b++) {
+            lst[a] = tmp[b];
+        }
+    }
+
 }
