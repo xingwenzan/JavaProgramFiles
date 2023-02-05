2f7eb8c418f28f3752b783714a23ce0eb410c5a5
86159 <zxw3180716935@163.com>
2023-02-05 23:21:04 +0800

    add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmTemplate/DataStructure/Main.java
+++ src/Algorithm/AlgorithmTemplate/DataStructure/Main.java
@@ -1,8 +1,35 @@
 package Algorithm.AlgorithmTemplate.DataStructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 /**
  * 用于测试数据结构的类
  */
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        SingleList singleList = new SingleList();
+        singleList.initialize();
+        String[] strings;
+        for (int i = 0; i < n; i++) {
+            strings = bufferedReader.readLine().split(" ");
+            if (Objects.equals(strings[0], "H")) {
+                int x = Integer.parseInt(strings[1]);
+                singleList.insert(0, x);
+            } else if (Objects.equals(strings[0], "D")) {
+                int x = Integer.parseInt(strings[1]);
+                singleList.delete(x);
+            } else {
+                int k = Integer.parseInt(strings[1]);
+                int x = Integer.parseInt(strings[2]);
+                singleList.insert(k, x);
+            }
+        }
+        singleList.out();
+    }
 
 }
--- src/Algorithm/AlgorithmTemplate/DataStructure/SingleList.java
+++ src/Algorithm/AlgorithmTemplate/DataStructure/SingleList.java
@@ -1,5 +1,35 @@
 package Algorithm.AlgorithmTemplate.DataStructure;
 
+import java.util.Arrays;
+
 public class SingleList {
     // 单链表 https://www.acwing.com/problem/content/828/
+    int N = 100010;
+    int[] values = new int[N];
+    int[] pointers = new int[N];
+    int idx = 1;
+
+    public void initialize() {
+        Arrays.fill(values, 0);
+        Arrays.fill(pointers, 0);
+    }
+
+    public void insert(int k, int x) {
+        values[idx] = x;
+        pointers[idx] = pointers[k];
+        pointers[k] = idx;
+        idx++;
+    }
+
+    public void delete(int k) {
+        pointers[k] = pointers[pointers[k]];
+    }
+
+    public void out() {
+        int i = pointers[0];
+        while (i != 0) {
+            System.out.printf("%d ", values[i]);
+            i = pointers[i];
+        }
+    }
 }
