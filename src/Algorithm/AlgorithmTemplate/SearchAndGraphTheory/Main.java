6c93b5317a86e9e512c0d0b87142adeea2db89bb
86159 <zxw3180716935@163.com>
2023-03-08 22:42:53 +0800

    add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmTemplate/SearchAndGraphTheory/Main.java
+++ src/Algorithm/AlgorithmTemplate/SearchAndGraphTheory/Main.java
@@ -7,9 +7,25 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // 树与图的广度优先遍历 图中点的层次
+        String[] strings = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+        PriorityTraversalOfTreesAndGraphs p = new PriorityTraversalOfTreesAndGraphs();
+        p.init();
+        for (int i = 0; i < m; i++) {
+            strings = bufferedReader.readLine().split(" ");
+            int father = Integer.parseInt(strings[0]);
+            int son = Integer.parseInt(strings[1]);
+            p.add(father, son);
+        }
+        System.out.println(p.bfs(1, n));
+
+        /*
+        // 树与图的深度优先遍历 树的重心
         int n = Integer.parseInt(bufferedReader.readLine());
         PriorityTraversalOfTreesAndGraphs p = new PriorityTraversalOfTreesAndGraphs();
-        p.dfsInit();
+        p.init();
         String[] strings;
         for (int i = 0; i < n - 1; i++) {
             strings = bufferedReader.readLine().split(" ");
@@ -19,6 +35,8 @@ public class Main {
             p.add(son, father);
         }
         System.out.println(p.centerOfGravityOfTree(n));
+
+         */
 
 
         /*
