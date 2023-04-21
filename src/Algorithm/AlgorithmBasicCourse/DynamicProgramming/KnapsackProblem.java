0e38c77ad50da140635a85ed9f7df8735d1f7c58
86159 <zxw3180716935@163.com>
2023-04-21 18:47:38 +0800

    add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmBasicCourse/DynamicProgramming/KnapsackProblem.java
+++ src/Algorithm/AlgorithmBasicCourse/DynamicProgramming/KnapsackProblem.java
@@ -23,11 +23,20 @@ public class KnapsackProblem {
     /*
     多重背包问题 II https://www.acwing.com/problem/content/5/
     多重背包的二进制优化方法，将 s 拆分成 1 2 4 8 …… 2^k …… 2^n s-2^n 个该物品的组合。然后 01
+    01 朴素版会 MLE，只能用 01 优化版
      */
 
-    private int idx = 0;
-    private final int N = 1000 * ((int) log2(2000) + 1) + 10;   // 多重 II = 11010；01、完全 = 1010；多重 I = 110
-    private final int[] V = new int[N], W = new int[N], S = new int[N];
+    /*
+    分组背包问题 https://www.acwing.com/problem/content/9/
+    f[i][j] = max(f[i-1][j], f[i-1][j-v[i][1]]+w[i][1], …… , f[i-1][j-v[i][s]]+w[i][s])
+     */
+
+    private int idx = 0;   // 01、完全、多重 I、多重 II
+    private final int N = 1000 * ((int) log2(2000) + 1) + 10;   // 多重 II = 11010；01、完全 = 1010；多重 I、分组 = 110
+    private final int[] V = new int[N], W = new int[N], S = new int[N];   // 01、完全、多重 I、多重 II
+    private int groupIdx = 0, objectIdx = 0;   // 分组
+    private final int[] SG = new int[N];   // 分组
+    private final int[][] VG = new int[N][N], WG = new int[N][N];   // 分组
 
 
     public static double log2(int x) {
@@ -60,6 +69,20 @@ public class KnapsackProblem {
             idx++;
             V[idx] = v * s;
             W[idx] = w * s;
+        }
+    }
+
+    public void addGroup(int s) {
+        groupIdx++;
+        SG[groupIdx] = s;
+    }
+
+    public void addObject(int s, int v, int w) {
+        objectIdx++;
+        VG[groupIdx][objectIdx] = v;
+        WG[groupIdx][objectIdx] = w;
+        if (objectIdx >= s) {
+            objectIdx = 0;
         }
     }
 
@@ -117,7 +140,7 @@ public class KnapsackProblem {
         return alls[bagVolume];
     }
 
-    public int naiveMultiple(int objectNum, int bagVolume) {
+    public int naiveMultipleI(int objectNum, int bagVolume) {
         int[][] alls = new int[N][N];
         for (int i = 0; i < N; i++) {
             Arrays.fill(alls[i], 0);
@@ -133,7 +156,53 @@ public class KnapsackProblem {
         return alls[objectNum][bagVolume];
     }
 
-    public int optimizationMultiple(int bagVolume) {
+    public int optimizationMultipleI(int objectNum, int bagVolume) {
+        int[] alls = new int[N];
+        Arrays.fill(alls, 0);
+        for (int i = 1; i <= objectNum; i++) {
+            for (int j = bagVolume; j >= V[i]; j--) {
+                for (int k = 1; k <= S[i] && k * V[i] <= j; k++) {
+                    alls[j] = Math.max(alls[j], alls[j - k * V[i]] + k * W[i]);
+                }
+            }
+        }
+        return alls[bagVolume];
+    }
+
+    public int MultipleII(int bagVolume) {
         return optimization01(idx, bagVolume);
+    }
+
+    public int naiveGroup(int nGroup, int bagVolume) {
+        int[][] alls = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(alls[i], 0);
+        }
+        for (int i = 1; i <= nGroup; i++) {
+            for (int j = 1; j <= bagVolume; j++) {
+                alls[i][j] = alls[i - 1][j];
+                for (int k = 1; k <= SG[i]; k++) {
+                    if (j >= VG[i][k]) {
+                        alls[i][j] = Math.max(alls[i][j], alls[i - 1][j - VG[i][k]] + WG[i][k]);
+                    }
+                }
+            }
+        }
+        return alls[nGroup][bagVolume];
+    }
+
+    public int optimizationGroup(int nGroup, int bagVolume) {
+        int[] alls = new int[N];
+        Arrays.fill(alls, 0);
+        for (int i = 1; i <= nGroup; i++) {
+            for (int j = bagVolume; j >= 1; j--) {
+                for (int k = 1; k <= SG[i]; k++) {
+                    if (j >= VG[i][k]) {
+                        alls[j] = Math.max(alls[j], alls[j - VG[i][k]] + WG[i][k]);
+                    }
+                }
+            }
+        }
+        return alls[bagVolume];
     }
 }
--- src/Algorithm/AlgorithmBasicCourse/DynamicProgramming/Main.java
+++ src/Algorithm/AlgorithmBasicCourse/DynamicProgramming/Main.java
@@ -7,7 +7,26 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        // 01背包问题、完全背包问题、多重背包问题 I
+        // 分组背包问题
+        String[] strings = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int v = Integer.parseInt(strings[1]);
+        KnapsackProblem knapsackProblem = new KnapsackProblem();
+        int s;
+        for (int i = 0; i < n; i++) {
+            s = Integer.parseInt(bufferedReader.readLine());
+            knapsackProblem.addGroup(s);
+            for (int j = 0; j < s; j++) {
+                strings = bufferedReader.readLine().split(" ");
+                knapsackProblem.addObject(s, Integer.parseInt(strings[0]), Integer.parseInt(strings[1]));
+            }
+        }
+        System.out.println(knapsackProblem.naiveGroup(n, v));
+        System.out.println(knapsackProblem.optimizationGroup(n, v));
+
+
+        /*
+        // 01背包问题、完全背包问题、多重背包问题 I、多重背包问题 II
         String[] strings = bufferedReader.readLine().split(" ");
         int n = Integer.parseInt(strings[0]);
         int v = Integer.parseInt(strings[1]);
@@ -25,7 +44,10 @@ public class Main {
         System.out.println(knapsackProblem.optimization01(n, v));
         System.out.println(knapsackProblem.naiveCompletely(n, v));
         System.out.println(knapsackProblem.optimizationCompletely(n, v));
-        System.out.println(knapsackProblem.naiveMultiple(n, v));
-        System.out.println(knapsackProblem.optimizationMultiple(v));
+        System.out.println(knapsackProblem.naiveMultipleI(n, v));
+        System.out.println(knapsackProblem.optimizationMultipleI(n,v));
+        System.out.println(knapsackProblem.MultipleII(v));
+
+         */
     }
 }
