5931861354dff8d7c7703024902f7b0bdf7aeadc
86159 <zxw3180716935@163.com>
2023-05-12 16:17:54 +0800

    add
    Signed-off-by: 86159 <zxw3180716935@163.com>

--- src/Algorithm/AlgorithmBasicCourse/Greed/IntervalProblem.java
+++ src/Algorithm/AlgorithmBasicCourse/Greed/IntervalProblem.java
@@ -2,13 +2,14 @@ package Algorithm.AlgorithmBasicCourse.Greed;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class IntervalProblem {
     // 区间选点 https://www.acwing.com/problem/content/907/\
     // 最大不相交区间数量 https://www.acwing.com/problem/content/910/
     ArrayList<int[]> interregional = new ArrayList<>();
 
-    // 区间选点、最大不相交区间数量
+    // 区间选点、最大不相交区间数量、区间分组
     public void add(int l, int r) {
         interregional.add(new int[]{l, r});
     }
@@ -34,5 +35,19 @@ public class IntervalProblem {
             }
         }
         return ans;
+    }
+
+    public int intervalGrouping() {
+        interregional.sort(Comparator.comparingInt(o -> o[0]));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int[] ints : interregional) {
+            if (heap.isEmpty() || heap.peek() >= ints[0]) {
+                heap.add(ints[1]);
+            } else {
+                heap.poll();
+                heap.add(ints[1]);
+            }
+        }
+        return heap.size();
     }
 }
--- src/Algorithm/AlgorithmBasicCourse/Greed/Main.java
+++ src/Algorithm/AlgorithmBasicCourse/Greed/Main.java
@@ -7,13 +7,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        // 区间选点、最大不相交区间数量
+        // 区间选点、最大不相交区间数量、区间分组
         IntervalProblem intervalProblem = new IntervalProblem();
         int n = Integer.parseInt(bufferedReader.readLine());
         for (int i = 0; i < n; i++) {
             String[] strings = bufferedReader.readLine().split(" ");
             intervalProblem.add(Integer.parseInt(strings[0]), Integer.parseInt(strings[1]));
         }
-        System.out.println(intervalProblem.intervalSelection());
+        System.out.println(intervalProblem.intervalSelection());   // 区间选点、最大不相交区间数量
+        System.out.println(intervalProblem.intervalGrouping());   // 区间分组
     }
 }
